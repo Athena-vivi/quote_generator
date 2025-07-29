accb63cac1b58fd0bc8ee5801a140d81821f1729
Vivi <viviwater159@gmail.com>
2025-07-29 18:34:29 +0800

    Initial commit of refactored project

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -3,14 +3,19 @@ import Script from "next/script"
 
 export function GoogleAnalytics() {
   return (
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QGY8EPYE2Q"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-QGY8EPYE2Q');
-    </script>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QGY8EPYE2Q"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QGY8EPYE2Q');
+        `}
+      </Script>
+    </>
   )
 }
